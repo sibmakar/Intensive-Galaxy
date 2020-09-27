5997277f3a01ff2995766c269054ad77007c6b5f
Sumit Singh <sumit.singh4613@gmail.com>
2020-09-27 14:46:05 +0530

    pdf invoice working

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,5 @@
 import logging
+import tempfile
 from datetime import datetime, timedelta
 
 from django.contrib import admin
@@ -6,9 +7,13 @@ from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.db.models import Count
 from django.db.models.functions import TruncDay
 from django.forms import forms, TypedChoiceField
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.template.loader import render_to_string
 from django.template.response import TemplateResponse
 from django.urls import path
 from django.utils.html import format_html
+from weasyprint import HTML
 
 from .models import (
     Product,
@@ -271,8 +276,6 @@ class DispatchersOrderAdmin(admin.ModelAdmin):
 
 # The class below will pass to the django admin templates a couple
 # of extra values that represent colors of headings
-
-
 class ColoredAdminSite(admin.sites.AdminSite):
     def each_context(self, request):
         context = super().each_context(request)
@@ -357,11 +360,48 @@ class ReportingColoredAdminSite(ColoredAdminSite):
         return super(ReportingColoredAdminSite, self).index(request, extra_context)
 
 
+# This mixin will be used for the invoice functionality, which is
+# only available to owners and employees, but not dispatchers
+class InvoiceMixin:
+    def get_urls(self):
+        urls = super(InvoiceMixin, self).get_urls()
+        my_urls = [
+            path(
+                "invoice/<int:order_id>/",
+                self.admin_view(self.invoice_for_order),
+                name="invoice",
+            )
+        ]
+
+        return my_urls + urls
+
+    def invoice_for_order(self, request, order_id):
+        order = get_object_or_404(Order, pk=order_id)
+
+        if request.GET.get("format") == "pdf":
+            html_string = render_to_string("invoice.html", {"order": order})
+            html = HTML(string=html_string, base_url=request.build_absolute_uri())
+
+            result = html.write_pdf()
+
+            response = HttpResponse(content_type="application/pdf")
+            response["Content-Disposition"] = "inline; filename=invoice.pdf"
+            response["Content-Transfer-Encoding"] = "binary"
+
+            with tempfile.NamedTemporaryFile(delete=True) as output:
+                output.write(result)
+                output.flush()
+                output = open(output.name, "rb")
+                binary_pdf = output.read()
+                response.write(binary_pdf)
+            return response
+
+        return render(request, "invoice.html", {"order": order})
+
+
 # Finally we define 3 instances of AdminSite, each with their own
 # set of required permissions and colors
-
-
-class OwnersAdminSite(ReportingColoredAdminSite):
+class OwnersAdminSite(InvoiceMixin, ReportingColoredAdminSite):
     site_header = "Intensive Galaxy Owners Administration"
     site_header_color = "black"
     module_caption_color = "grey"
@@ -370,7 +410,7 @@ class OwnersAdminSite(ReportingColoredAdminSite):
         return request.user.is_active and request.user.is_superuser
 
 
-class CentralOfficeAdminSite(ReportingColoredAdminSite):
+class CentralOfficeAdminSite(InvoiceMixin, ReportingColoredAdminSite):
     site_header = "Intensive Galaxy Central Office Administration"
     site_header_color = "purple"
     module_caption_color = "pink"
--- main/tests/test_admin.py
+++ main/tests/test_admin.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from decimal import Decimal
+from unittest.mock import patch
+
 from django.test import TestCase
 from django.urls import reverse
 
@@ -33,3 +37,50 @@ class TestAdminViews(TestCase):
         data = dict(zip(response.context["labels"], response.context["values"]))
 
         self.assertEqual(data, {"B": 3, "C": 2, "A": 6})
+
+    def test_invoice_renders_exactly_as_expected(self):
+        # TODO: test case failing
+        products = [
+            factories.ProductFactory(
+                name="The cathedral and the bazaar",
+                active=True,
+                price=Decimal("500.00"),
+            ),
+            factories.ProductFactory(
+                name="The cathedral and the bazaar",
+                active=True,
+                price=Decimal("500.00"),
+            ),
+        ]
+
+        with patch("django.utils.timezone.now") as mock_now:
+            mock_now.return_value = datetime(2020, 9, 27, 12, 00, 00)
+
+            order = factories.OrderFactory(
+                id=1,
+                billing_name="sumit",
+                billing_address1="new",
+                billing_address2="new",
+                billing_pin_code="201306",
+                billing_city="noida",
+                billing_country="in",
+            )
+
+            factories.OrderLineFactory.create_batch(1, order=order, product=products[0])
+            factories.OrderLineFactory.create_batch(1, order=order, product=products[1])
+            user = User.objects.create_superuser("user2", "pw432joij")
+            self.client.force_login(user)
+            response = self.client.get(
+                reverse("admin:invoice", kwargs={"order_id": order.id})
+            )
+
+            self.assertEqual(response.status_code, 200)
+
+            pdf_response = self.client.get(
+                reverse("admin:invoice", kwargs={"order_id": order.id}),
+                {"format": "pdf"},
+            )
+            content = pdf_response.content
+            with open("main/fixtures/invoice.pdf", "rb") as fixture:
+                expected_content = fixture.read()
+            self.assertEqual(content, expected_content)
