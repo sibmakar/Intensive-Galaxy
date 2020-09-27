b8712fe7f20990e1de95ac2ff571546caeed3d8f
Sumit Singh <sumit.singh4613@gmail.com>
2020-09-27 10:52:45 +0530

    adds order models

--- main/models.py
+++ main/models.py
@@ -1,8 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.core import exceptions
+from django.core.exceptions import ValidationError
+from django.core.handlers import exception
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+
 from main.managers import ActiveManager, ProductTagManager, UserManager
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class TimeStampedModel(models.Model):
@@ -127,6 +135,44 @@ class Cart(TimeStampedModel):
     def count(self):
         return sum(i.quantity for i in self.productincart_set.all())
 
+    def create_order(self, billing_address, shipping_address):
+        if not self.user:
+            raise ValidationError("Cannot create order without user")
+
+        logger.info(
+            "Creating Order for basket_id=%d"
+            ", shipping_address_id=%d, billing_address_id=%d",
+            self.id,
+            shipping_address.id,
+            billing_address.id,
+        )
+        order_data = {
+            "user": self.user,
+            "billing_name": billing_address.name,
+            "billing_address1": billing_address.address1,
+            "billing_address2": billing_address.address2,
+            "billing_pin_code": billing_address.pin_code,
+            "billing_city": billing_address.city,
+            "billing_country": billing_address.country,
+            "shipping_name": shipping_address.name,
+            "shipping_address1": shipping_address.address1,
+            "shipping_address2": shipping_address.address2,
+            "shipping_pin_code": shipping_address.pin_code,
+            "shipping_city": shipping_address.city,
+            "shipping_country": shipping_address.country,
+        }
+        order = Order.objects.create(**order_data)
+        c = 0
+        for product in self.productincart_set.all():
+            for item in range(product.quantity):
+                order_line_data = {"order": order, "product": product.product}
+                order_line = OrderLine.objects.create(**order_line_data)
+                c += 1
+            logger.info("Created order with id=%d and lines_count=%d", order.id, c)
+        self.status = Cart.SUBMITTED
+        self.save()
+        return order
+
     def __str__(self):
         return f"{self.user.first_name}'s Cart"
 
@@ -135,3 +181,49 @@ class ProductInCart(TimeStampedModel):
     cart = models.ForeignKey(to=Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
+
+
+class Order(TimeStampedModel):
+    """Order Model"""
+
+    NEW = 10
+    PAID = 20
+    DONE = 30
+    STATUSES = ((NEW, "New"), (PAID, "Paid"), (DONE, "Done"))
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=STATUSES, default=NEW)
+    billing_name = models.CharField(max_length=60)
+    billing_address1 = models.CharField(max_length=60)
+    billing_address2 = models.CharField(max_length=60, blank=True)
+    billing_pin_code = models.CharField(max_length=12)
+    billing_city = models.CharField(max_length=60)
+    billing_country = models.CharField(max_length=3)
+    shipping_name = models.CharField(max_length=60)
+    shipping_address1 = models.CharField(max_length=60)
+    shipping_address2 = models.CharField(max_length=60, blank=True)
+    shipping_pin_code = models.CharField(max_length=12)
+    shipping_city = models.CharField(max_length=60)
+    shipping_country = models.CharField(max_length=3)
+
+    def __str__(self):
+        return f"{self.user.first_name}'s Order"
+
+
+class OrderLine(TimeStampedModel):
+    """Order Line Model"""
+
+    NEW = 10
+    PROCESSING = 20
+    SENT = 30
+    CANCELLED = 40
+    STATUSES = (
+        (NEW, "New"),
+        (PROCESSING, "Processing"),
+        (SENT, "Sent"),
+        (CANCELLED, "Cancelled"),
+    )
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="lines")
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    status = models.IntegerField(choices=STATUSES, default=NEW)
--- main/tests/test_models.py
+++ main/tests/test_models.py
@@ -1,15 +1,60 @@
 from decimal import Decimal
 from django.test import TestCase
-from main.models import Product
+from main import models
 
 
 class TestProduct(TestCase):
     def test_active_manager_works(self):
-        Product.objects.create(
+        models.Product.objects.create(
             name="The cathedral and the bazaar", price=Decimal("10.00")
         )
-        Product.objects.create(name="Pride and Prejudice", price=Decimal("2.00"))
-        Product.objects.create(
+        models.Product.objects.create(name="Pride and Prejudice", price=Decimal("2.00"))
+        models.Product.objects.create(
             name="A Tale of Two Cities", price=Decimal("2.00"), active=False
         )
-        self.assertEqual(len(Product.objects.active()), 2)
+        self.assertEqual(len(models.Product.objects.active()), 2)
+
+
+class TestCart(TestCase):
+    def test_create_order_works(self):
+        p1 = models.Product.objects.create(
+            name="The cathedral and the bazaar",
+            price=Decimal("10.00"),
+        )
+        p2 = models.Product.objects.create(
+            name="Pride and Prejudice", price=Decimal("2.00")
+        )
+        user1 = models.User.objects.create_user("user1", "pw432joij")
+        billing = models.Address.objects.create(
+            user=user1,
+            name="John Kimball",
+            address1="127 Strudel road",
+            city="London",
+            country="uk",
+        )
+        shipping = models.Address.objects.create(
+            user=user1,
+            name="John Kimball",
+            address1="123 Deacon road",
+            city="London",
+            country="uk",
+        )
+        cart = models.Cart.objects.create(user=user1)
+        models.ProductInCart.objects.create(cart=cart, product=p1)
+        models.ProductInCart.objects.create(cart=cart, product=p2)
+
+        with self.assertLogs("main.models", level="INFO") as cm:
+            order = cart.create_order(billing, shipping)
+
+        self.assertGreaterEqual(len(cm.output), 1)
+
+        order.refresh_from_db()
+
+        self.assertEqual(order.user, user1)
+        self.assertEqual(order.billing_address1, "127 Strudel road")
+        self.assertEqual(order.shipping_address1, "123 Deacon road")
+
+        self.assertEqual(order.lines.all().count(), 2)
+        lines = order.lines.all()
+        self.assertEqual(lines[0].product, p1)
+        self.assertEqual(lines[1].product, p2)
